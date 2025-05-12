fa6d1e78fc91a8904f8aea6238e7d2e4c5aace6e
amit-prajapati-ap <prajapatiapamit@gmail.com>
2025-05-12 21:41:06 +0530

    feat: Add Stripe webhook handling for payment intents and update course enrollment logic

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import { clerkWebhooks } from './controllers/webhooks.controller.js'
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.controller.js'
 import { clerkMiddleware } from '@clerk/express'
 
 const app = express()
@@ -17,6 +17,7 @@ app.use(clerkMiddleware());
 app.get('/', (_, res) => res.send("API working"))
 
 app.post('/clerk', express.json(), clerkWebhooks)
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 app.use(express.json({ limit: '25kb' }))
 app.use(express.urlencoded({extended: true, limit: '25kb'}))
